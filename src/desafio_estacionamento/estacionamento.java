18724bea67dd5065e2e7befae59a42fccd903bf8
Incode <incode@CEUMA.EDU.BR>
2024-09-30 21:41:03 -0300

    first commit

--- src/desafio_estacionamento/estacionamento.java
+++ src/desafio_estacionamento/estacionamento.java
@@ -1,4 +1,50 @@
 package desafio_estacionamento;
 
-public class estacionamento {
+class GeradorDeTicket {
+    private int numeroDoTicket;
+    private final double valorDoTicket;
+    private boolean estaPago;
+    private double receitaTotal;
+
+    public GeradorDeTicket(double valorDoTicket) {
+        this.valorDoTicket = valorDoTicket;
+        this.numeroDoTicket = 1;
+        this.estaPago = false;
+        this.receitaTotal = 0.0;
+    }
+
+    public void emitirTicket() {
+        System.out.println("Ticket emitido: #" + numeroDoTicket + " - Valor: " + valorDoTicket);
+        numeroDoTicket++;
+    }
+
+    public void pagarTicket(double pagamento) {
+        if (!estaPago) {
+            if (pagamento >= valorDoTicket) {
+                estaPago = true;
+                receitaTotal += valorDoTicket;
+                System.out.println("Ticket pago com sucesso!");
+            } else {
+                System.out.println("Pagamento insuficiente. Por favor, tente novamente.");
+            }
+        } else {
+            System.out.println("Ticket já foi pago.");
+        }
+    }
+
+    public void verificarSaldo() {
+        System.out.println("Receita total: " + receitaTotal);
+    }
+
+    public static void main(String[] args) {
+        GeradorDeTicket maquina = new GeradorDeTicket(10.0); // Inicialize com um valor fixo de ticket de 10.0
+
+        maquina.emitirTicket();
+        maquina.pagarTicket(15.0);
+        maquina.verificarSaldo();
+
+        maquina.emitirTicket();
+        maquina.pagarTicket(5.0);
+        maquina.verificarSaldo();
+    }
 }
